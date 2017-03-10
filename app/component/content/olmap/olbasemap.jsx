6520fa4061b7b48c6629d1acba339ea98f845ce9
TechGIS <742311374@qq.com>
2017-03-10 15:36:40 +0800

    write some example of openlayers 3 of tianmap use ol3 and tianmap map service to create Olbasemap component.

--- app/component/content/olmap/olbasemap.jsx
+++ app/component/content/olmap/olbasemap.jsx
@@ -14,42 +14,42 @@ class Olbasemap extends React.Component{
 
 	componentDidMount(){
 		util.adaptHeight('map',105,300);//高度自适应
-		let projection ;
-		projection = ol.proj.get('EPSG:4326');
+
+		let projection,attribution,coor,view;
+
+        attribution = new ol.Attribution({
+            html: '© <a href="http://www.chinaonmap.com/map/index.html">天地图</a>'
+        });
+
 		let map = new ol.Map({
-      	    target: 'map',
-      	    layers: [
-      	        new ol.layer.Tile({
-      	        	// opacity:1,
-      	        	source:new ol.source.WMTS({
-       	        		// url:"http://t0.tianditu.com/vec_c/wmts",
-       	        		url: 'http://demo.opengeo.org/geoserver/wms',
-       	        		params: {LAYERS: 'nasa:bluemarble', VERSION: '1.1.1'}
-       	        		// opacity:1,
-       	        		// projection: projection,
-      	      		})
-      	        })
-      	      
-      	    ],
-      	    view: new ol.View({
-      	      projection: 'EPSG:4326',//WGS84
-      	      center: ol.proj.fromLonLat([104, 30]),
-      	      zoom: 5,
-      	    })
+            target: 'map',
+            layers: [
+                new ol.layer.Tile({
+                    source: new ol.source.XYZ({
+                        attributions: [attribution],
+                        url: "http://t2.tianditu.com/DataServer?T=vec_w&x={x}&y={y}&l={z}"
+                    })
+                }),
+                new ol.layer.Tile({
+                    source: new ol.source.XYZ({
+                        url: "http://t2.tianditu.com/DataServer?T=cva_w&x={x}&y={y}&l={z}"
+                    })
+                })
+            ],
+            view: new ol.View({
+              // projection: 'EPSG:4326',//WGS84
+              center: ol.proj.fromLonLat([104, 30]),
+              zoom: 5,
+            }),
+            controls: ol.control.defaults().extend([
+                new ol.control.FullScreen(), //全屏控件
+                new ol.control.ScaleLine(), //比例尺
+                new ol.control.OverviewMap(), //鹰眼控件
+                new ol.control.Rotate(),
+                new ol.control.MousePosition(),
+                new ol.control.ZoomSlider(),
+             ]),
       	});
-		/*let map = new ol.Map({
-      	    target: 'map',
-      	    layers: [
-      	      new ol.layer.Tile({
-       	        source: new ol.source.OSM()
-      	      })
-      	    ],
-      	    view: new ol.View({
-      	      // projection: 'EPSG:4326',//WGS84
-      	      center: ol.proj.fromLonLat([104, 30]),
-      	      zoom: 5,
-      	    })
-      	});*/
 	}
 	render(){
 		return(
