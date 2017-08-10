3585a20c8b96ed8bb62205376636f57da3be8886
zrysmt <742311374@qq.com>
2017-08-10 15:47:05 +0800

    update

---
+++ app/component/content/infoModal/infoModal.jsx
@@ -0,0 +1,21 @@
+import './infoModal.scss';
+
+import React from 'react';
+
+class InfoModal extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    componentDidMount() {
+    }
+
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+
+export default InfoModal;
