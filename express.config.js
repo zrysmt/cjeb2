c96421270010a346692eff5f97c2508b771d4bb0
zrysmt <742311374@qq.com>
2018-01-04 21:01:30 +0800

    update server

--- express.config.js
+++ express.config.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var sqlite3 = require('sqlite3').verbose();
+var async = require('async');
 
 var app = express();
 var db = new sqlite3.Database('../db/CJEB.db');
@@ -78,10 +79,128 @@ app.get('/getjson/:city/:year/:ind',function(req,res){
         });
     });
 })
-//某个城市某一年某几个指标
-//如：/getjson/上海市/2014/GDP增长率,GDP
-// TODO
+//某个城市某一年某个指标 通过指标id找
+//所有城市某一年的某个指标
+//如：byindid/allcity/2014/102
+app.get('/byindid/:city/:year/:indId',function(req,res){
+    var params = req.params;
+    if(!params.indId) res.send('indId not found');
 
+    //查到指标对应的字段
+    db.all("SELECT field,tabname,fieldRealname,unit FROM fieldsdef WHERE id = '"+ 
+          params.indId +"' limit 1", function(err, rows) {
+        
+        if(err) res.send(err);
+
+        rows.forEach(function (row) {
+            var filter =  "V4= '"+ params.year+"'"; 
+            if(params.city != 'allcity')
+                filter += "and V2='"+params.city+"'";
+
+            db.all("SELECT a.FID as id,a.V4 as year,"+row.field+
+                " as value,b.cityName as cityName,b.cityCode as cityCode,b.cityProv as province,b.lat as lat,b.lng as lng FROM'"+
+                row.tabname+"' as a LEFT JOIN t_allCitys as b ON a.V2 = b.cityName WHERE "+
+                filter,function(err1,rows1){
+                    
+                    if(err1)    res.send(err1);
+                    rows1.forEach(function(row1){
+                        row1.unit = row.unit;
+                        row1.name = row.fieldRealname;
+                    })
+                    res.json(rows1);
+                    // closeDb();
+            })
+        });
+    });
+})
+//某个城市某一年某几个指标
+//最后按指标显示[[],[],[],[]]
+//如：/byindids/上海市/2014/101,102,103,143
+//所有城市某一年的某个指标
+//如：byindids/allcity/2014/101,102,103,143
+app.get('/byindids/inds/:city/:year/:indId',function(req,res){
+    var params = req.params;
+    if(!params.indId) res.send('indId not found');
+    var resRow = {};
+    //查到指标对应的字段
+    db.all("SELECT field,tabname,fieldRealname,unit FROM fieldsdef WHERE id in ("+ 
+          params.indId +")", function(err, rows) {
+        if(err) res.send(err);
+        
+        var filter =  "V4= '"+ params.year+"'"; 
+        if(params.city != 'allcity')
+            filter += "and V2='"+params.city+"'";
+
+        async.map(rows,function(row,callback){
+            db.all("SELECT a.FID as id,a.V4 as year,"+row.field+
+                " as value,b.cityName as cityName,b.cityCode as cityCode,b.cityProv as province,b.lat as lat,b.lng as lng FROM'"+
+                row.tabname+"' as a LEFT JOIN t_allCitys as b ON a.V2 = b.cityName WHERE "+
+                filter,function(err1,rows1){
+                    
+                    if(err1)    res.send(err1);
+                    rows1.forEach(function(row1){
+                        row1.unit = row.unit;
+                        row1.name = row.fieldRealname;
+                    })
+                    callback(null,rows1);
+            });
+        },function(asyncErr,results){
+            res.json(results);
+        })
+
+    });
+});
+//某个城市某一年某几个指标
+//最后按城市显示[[],[],[],[],...,[]] 按照城市进行分类
+//如：/byindids/citys/上海市/2014/101,102,103,143
+//所有城市某一年的某个指标
+//如：byindids/citys/allcity/2014/101,102,103,143
+app.get('/byindids/citys/:city/:year/:indId',function(req,res){
+    var params = req.params;
+    if(!params.indId) res.send('indId not found');
+    var resRow = {};
+    //查到指标对应的字段
+    db.all("SELECT field,tabname,fieldRealname,unit FROM fieldsdef WHERE id in ("+ 
+          params.indId +")", function(err, rows) {
+        if(err) res.send(err);
+        
+        var filter =  "V4= '"+ params.year+"'"; 
+        if(params.city != 'allcity')
+            filter += "and V2='"+params.city+"'";
+
+        async.map(rows,function(row,callback){
+            db.all("SELECT a.FID as id,a.V4 as year,"+row.field+
+                " as value,b.cityName as cityName,b.cityCode as cityCode,b.cityProv as province,b.lat as lat,b.lng as lng FROM'"+
+                row.tabname+"' as a LEFT JOIN t_allCitys as b ON a.V2 = b.cityName WHERE "+
+                filter,function(err1,rows1){
+                    
+                    if(err1)    res.send(err1);
+                    rows1.forEach(function(row1){
+                        row1.unit = row.unit;
+                        row1.name = row.fieldRealname;
+                    })
+                    callback(null,rows1);
+            });
+        },function(asyncErr,results){
+            var obj = {},res1 = {};
+            results.forEach(function(items,indexs){
+                items.forEach(function(it,ind){
+                    if(it.cityName){
+                        if(obj[it.cityName]){
+                            res1[it.cityName].push(it);
+                        }else{
+                            res1[it.cityName] = [];
+                            res1[it.cityName].push(it);
+                            obj[it.cityName] = true;
+                        }                        
+                    }
+                })
+            })
+            res.json(res1);
+        })
+
+    });
+})
 //插入数据库的例子，带回调
 function insertRows() {
     console.log("insertRows Ipsum i");
